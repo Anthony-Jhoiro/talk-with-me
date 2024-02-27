c4b95a7aa86316c11feb23c8e44295232579c1c2
anthony <47711333+Anthony-Jhoiro@users.noreply.github.com>
2024-02-27 14:39:23 +0100

    :tada: add admin web client

---
+++ admin-webclient/src/components/Scene3D.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import Object3D from './Object3D.vue';
+import {TresCanvas} from "@tresjs/core"
+
+const { objectFile, textureFile } = defineProps<{
+  objectFile: string;
+  textureFile: string;
+}>();
+</script>
+
+<template>
+  <TresCanvas :alpha="true">
+    <TresPerspectiveCamera :position="[0.4, 1.5, -2]" :look-at="[0, 0.75, 0]" />
+    <Suspense>
+      <!-- Object is handled in another component to avoid server-side rendering issues -->
+      <Object3D :object-file="objectFile" :texture-file="textureFile" />
+    </Suspense>
+    <TresAmbientLight :intensity="2" />
+  </TresCanvas>
+</template>
+
+<style scoped></style>
---
+++ admin-webclient/src/views/TalkView.vue
@@ -0,0 +1,110 @@
+<script setup lang="ts">
+
+import { onMounted } from '@vue/runtime-core';
+import { type GetCompanionMessagesBody, getCompanionWithMessages } from '../api/companions';
+import Message from "../components/Message.vue"
+import { computed, ref } from 'vue';
+
+const {id} = defineProps<{
+  id: string
+}>()
+
+const companion = ref<GetCompanionMessagesBody>()
+const loadingCompanionErrorMessage = ref<string>()
+const isLoadingCompanion = ref(true)
+
+
+onMounted(() => {
+  getCompanionWithMessages(id)
+    .then((c: GetCompanionMessagesBody) => companion.value = c)
+    .catch((e: any) => loadingCompanionErrorMessage.value = e)
+    .finally(() => isLoadingCompanion.value = false)
+})
+
+const formInput = ref('');
+const sendingMessage = ref<string | null>(null);
+
+const messages = computed(() => {
+  if (!companion.value) {
+    return [];
+  }
+
+  if (!sendingMessage.value) {
+    return [...companion.value.messages];
+  }
+
+  return [
+    ...companion.value.messages,
+    {
+      id: 'unknown',
+      message: sendingMessage,
+      createdAt: new Date().toISOString(),
+    },
+  ];
+});
+
+async function onNewMessage(e: any) {
+  e.preventDefault();
+  sendingMessage.value = formInput.value;
+  //await sendMessage(companion.value!.companion.id, formInput.value);
+
+  // Reset input
+  formInput.value = '';
+  //await refresh();
+  sendingMessage.value = null;
+}
+
+const waitingForResponse = computed(() => sendingMessage.value !== null);
+
+</script>
+
+<template>
+
+    <div v-if="isLoadingCompanion">
+      <LoadingIndicator />
+    </div>
+
+    <p v-if="loadingCompanionErrorMessage" class="text-maroon">
+      {{ loadingCompanionErrorMessage }}
+    </p>
+
+    <template v-if="companion">
+      <div id="companion-header" class="flex">
+        <div class="size-52">
+          <Scene3D object-file="/vulpis.obj" texture-file="/vulpis.png" />
+        </div>
+
+        <h2 class="text-4xl font-semibold">{{ companion.companion.name }}</h2>
+      </div>
+
+      <p class="text-subtext0 border-l-4 p-5 border-subtext0 mb-5">
+        {{ companion.companion.background }}
+      </p>
+
+      <section id="discussion">
+        <Message v-for="message in messages" :message="message" />
+      </section>
+
+      <div class="flex justify-center my-5" v-if="waitingForResponse">
+        <LoadingIndicator />
+      </div>
+
+      <form @submit="onNewMessage">
+        <div>
+          <input
+            :disabled="waitingForResponse"
+            class="bg-surface1 text-text px-8 py-3 outline-4 outline-crust focus:outline-teal w-full"
+            id="message-input"
+            name="message"
+            type="text"
+            v-model="formInput"
+            placeholder="Your question here"
+          />
+        </div>
+      </form>
+    </template>
+</template>
+
+<style scoped>
+
+</style>
